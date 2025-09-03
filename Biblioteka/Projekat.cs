cbf66e5068a656383df542959f6db08124f5bad5
Nemanja <Nemanja@DESKTOP-SEMRPKP>
2025-09-04 00:28:04 +0200

    Klase

---
+++ Biblioteka/Projekat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+
+    [Serializable]
+    public enum StanjeProjekta { naCekanju, uIzradi, zavrseno }
+    public class Projekat
+    {
+        public string NazivProjekta { get; set; }
+        public StanjeProjekta stanje { get; set; }
+    }
+}
