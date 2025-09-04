f29431d339092b01a508139a63ea988dbea4416f
HP <HP@DESKTOP-LTSDL4R>
2025-09-04 21:28:18 +0200

    Dodat recnik i kreiran UDP soket na serveru.

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using Biblioteka;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    internal class Server
+    public class Server
     {
+        public Dictionary<string, List<ZadatakProjekta>> projekti = new Dictionary<string, List<ZadatakProjekta>>();
         static void Main(string[] args)
         {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram,
+            ProtocolType.Udp);
+            //IPEndPoint destinationEP = new IPEndPoint(IPAddress.Parse(""), );
+
         }
     }
 }
+
