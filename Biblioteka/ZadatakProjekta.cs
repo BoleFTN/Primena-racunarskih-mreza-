32485bc35308c299ba9914104f4513c8c9263565
HP <HP@DESKTOP-LTSDL4R>
2025-09-04 21:30:40 +0200

    Dopuna klase ZadatakProjekta.

---
+++ Biblioteka/ZadatakProjekta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteka
+{
+
+    [Serializable]
+    public enum StanjeProjekta { naCekanju, uIzradi, zavrseno }
+    public class ZadatakProjekta
+    {
+        public string NazivProjekta { get; set; }
+        public string zaposleni {  get; set; }
+        string rok;
+        int prioritet;
+        public StanjeProjekta stanje { get; set; }
+    }
+}
