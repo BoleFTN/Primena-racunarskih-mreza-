e20e95d61f3ccdf9e82fa9b558d562c609824bff
HP <HP@DESKTOP-LTSDL4R>
2025-09-08 23:14:15 +0200

    Server salje informacije o TCP uticnici

--- Menadzer/Menadzer.cs
+++ Menadzer/Menadzer.cs
@@ -13,33 +13,44 @@ namespace Menadzer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Menadzer krece sa radom...");
             Socket UDPmenadzerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint UDPdestinationEP = new IPEndPoint(IPAddress.Loopback, 50001);
-            EndPoint serverEP = new IPEndPoint(IPAddress.Any, 50000);
+            IPEndPoint UDPdestinationEP = new IPEndPoint(IPAddress.Loopback, 27015);
+           // EndPoint serverEP = new IPEndPoint(IPAddress.Any, 50000);
+              EndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
 
-            string ImeMenadzera = ""; 
-
-            if (File.Exists("Menadzer.txt"))
-            {
-                //u Menadzer.txt se cuva ulogovani korisnik
-                ImeMenadzera = File.ReadAllText("Menadzer.txt");
-                if (ImeMenadzera.Trim().Equals(string.Empty))
+                string ImeMenadzera = "";
+                if (File.Exists("Menadzer.txt"))
                 {
-                    Console.WriteLine("Unesite vase korisnicko ime da bi ste dobili uticnicu za rad u formatu MENADZER:[VASE IME]");
-                    ImeMenadzera = Console.ReadLine();
+                    //u Menadzer.txt se cuva ulogovani korisnik
+                    ImeMenadzera = File.ReadAllText("Menadzer.txt");
+                    if (ImeMenadzera.Trim().Equals(string.Empty))
+                    {
+                        Console.WriteLine("Unesite vase korisnicko ime da bi ste dobili uticnicu za rad u formatu MENADZER:[VASE IME]");
+                        ImeMenadzera = Console.ReadLine();
+                    }
+                    //sada prosledi ImeMenadzera serveru i trebalo bi da dobijes koju tcp uticnicu koristis
+
+                    byte[] enkriptovanaPoruka = Encoding.UTF8.GetBytes(ImeMenadzera);
+                    int slanje = UDPmenadzerSocket.SendTo(enkriptovanaPoruka, 0, enkriptovanaPoruka.Length, SocketFlags.None, UDPdestinationEP);
+
                 }
-                //sada prosledi ImeMenadzera serveru i trebalo bi da dobijes koju tcp uticnicu koristis
-                byte[] enkriptovanaPoruka = Encoding.UTF8.GetBytes(ImeMenadzera);
-                int slanje = UDPmenadzerSocket.SendTo(enkriptovanaPoruka,0,enkriptovanaPoruka.Length, SocketFlags.None,serverEP );
-            }
-            else {
-                Console.WriteLine("Unesite vase korisnicko ime u formatu MENADZER:[VASE IME]");
-                ImeMenadzera = Console.ReadLine();
-                File.WriteAllText("Menadzer.txt", ImeMenadzera);
-                //poslati serveru username da bi dobili uticnicu
-                byte[] enkriptovanaPoruka = Encoding.UTF8.GetBytes(ImeMenadzera);
-                int slanje = UDPmenadzerSocket.SendTo(enkriptovanaPoruka, 0, enkriptovanaPoruka.Length, SocketFlags.None, serverEP);
-            }
+                else
+                {
+                    Console.WriteLine("Unesite vase korisnicko ime u formatu MENADZER:[VASE IME]");
+                    ImeMenadzera = Console.ReadLine();
+                    File.WriteAllText("Menadzer.txt", ImeMenadzera);
+                    //poslati serveru username da bi dobili uticnicu
+                    byte[] enkriptovanaPoruka = Encoding.UTF8.GetBytes(ImeMenadzera);
+                    int slanje = UDPmenadzerSocket.SendTo(enkriptovanaPoruka, 0, enkriptovanaPoruka.Length, SocketFlags.None, UDPdestinationEP);
+
+                }
+                //Menadzer prima informacije o TCP uticnici koje mu salje server
+                byte[] prijemniBuffer = new byte[1024];
+                int brBajta = UDPmenadzerSocket.ReceiveFrom(prijemniBuffer, ref serverEP);
+                string poruka_o_tcp_uticnici = Encoding.UTF8.GetString(prijemniBuffer, 0, brBajta);
+                Console.WriteLine($"Konektovanje sa serverom uspesno, port tcp uticnice je {poruka_o_tcp_uticnici}");
+               
         }
     }
 }
